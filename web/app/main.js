93e5c37f251bbca730838482ec6b818737f6cb38
Agustina <agustinagarro97@gmail.com>
2021-03-23 15:31:53 +0100

    main js re-making, html completing and editing. controllers, get object and return status, a little bit of css

--- web/app/main.js
+++ web/app/main.js
@@ -1,23 +1,46 @@
 angular
-    .module("ohm-delivery", [])
-    .controller("tracking", function($scope, $http) {
-        const NOT_FOUND = (status, id) => (status === 404) ? `Sorry, we haven't found your tracking ID {id}.`: ERROR();
-        const ERROR = () =>'Oops, this website is under construction, please come back later. ';
-        $scope.formData = {};
-        $scope.get = function() {
-            if (this.trackingId) {
-            $http.get(`/ohms/${this.trackingId}`)
-            .then((result) => {
-                $scope.result = result.data;
-                $scope.formData = {};
-                $scope.errorMessage = '';
+	.module('ohm-delivery', [])
+	.controller('tracking', function($scope, $http) {
+		const NOT_FOUND = (status, id) => (status === 404) ? `Sorry, the tracking ID #${id} was not found.` : ERROR();
+		const ERROR = () => 'Oops, this website is under construction, please come back later.';
+		$scope.formData = {};
+		$scope.get = function() {
+			if (this.trackingId) {
+			        $http.get(`/ohms/${this.trackingId}`)
+					.then((result) => {
+						$scope.result = result.data;
+						$scope.formData = {};
+						$scope.errorMessage = '';
+					}, (error) => {
+						$scope.result = null;
+						$scope.errorMessage = NOT_FOUND(error.status, this.trackingId);
+					});
+			} else {
+				
+				$scope.errorMessage = 'Please choose a tracking ID.';
+			}
+		};
+            
+           
+    
+    $scope.updateStatus = function(statusCode) {
+        if (this.trackingId) {
+            $http.put(`/ohms/${this.trackingId}/status`, {
+                code: statusCode,
                 
-            }, (err) => {
+            }).then((result) => {
+                $scope.result = result.data;
+                $scope.errorMessage = '';
+            }, (error) => {
                 $scope.result = null;
-                $scope.errorMessage = NOT_FOUND(err.status, this.trackingId);
+                $scope.errorMessage = NOT_FOUND(error.status, $scope.trackingId);
             });
-        } else {
-            $scope.errorMessage = 'Select a tracking ID.';
         }
-    }
-        });
+    };
+
+
+ });
+
+
+
+    
