f088d801c005cc9dbb882d8fd9b24b9c8a549754
Agustina <agustinagarro97@gmail.com>
2021-03-23 12:55:44 +0100

    main.js controller and module. re-ordering and re writing some code

--- web/app/main.js
+++ web/app/main.js
@@ -1,12 +1,23 @@
 angular
     .module("ohm-delivery", [])
     .controller("tracking", function($scope, $http) {
-        $scope.sendData = function() {
+        const NOT_FOUND = (status, id) => (status === 404) ? `Sorry, we haven't found your tracking ID {id}.`: ERROR();
+        const ERROR = () =>'Oops, this website is under construction, please come back later. ';
+        $scope.formData = {};
+        $scope.get = function() {
+            if (this.trackingId) {
             $http.get(`/ohms/${this.trackingId}`)
-            .then((error) => {
-                this.errorMessage = 'Oops, this website is under construction, please come back later.';
-            }, (result) => {
-                this.errorMessage = '';
+            .then((result) => {
+                $scope.result = result.data;
+                $scope.formData = {};
+                $scope.errorMessage = '';
+                
+            }, (err) => {
+                $scope.result = null;
+                $scope.errorMessage = NOT_FOUND(err.status, this.trackingId);
             });
-        };
-    });
+        } else {
+            $scope.errorMessage = 'Select a tracking ID.';
+        }
+    }
+        });
